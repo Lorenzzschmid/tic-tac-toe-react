adc64b0577e7a57adfffdf81ebfc1d27f50a0fcd
Lorenzzschmid <lorenz-schmid@gmx.net>
2022-06-29 21:29:26 +0200

    added the X in the clickEvent

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React, {useState} from 'react'; 
+import React, { useState } from "react";
 import "./App.css";
 
 const Square = (props) => {
- return (
-    <button
-      className="square"
-      onClick={() => {}}
-    >
+  return (
+    <button className="square" onClick={props.onClickEvent}>
       {props.value}
     </button>
   );
 };
 
 const Board = () => {
-  const initialSquares = [
-    null, null, null,
-    null, null, null,
-    null, null, null,
-  ];
+  const initialSquares = [null, null, null, null, null, null, null, null, null];
   const [squares, setSquares] = useState(initialSquares);
 
+  const handleClickEvent = (i) => {
+    // 1. Make a copy of the squares state array.
+    const newSquares = [...squares];
+    // 2. Mutate the copy, setting the i-th element to 'X.
+    newSquares[i] = "X";
+    // 3. Call the setSquares function with the mutated copy.
+    setSquares(newSquares);
+  };
+
   const renderSquare = (i) => {
-    return <Square value={squares[i]}/>;
+    return (
+      <Square value={squares[i]} onClickEvent={() => handleClickEvent(i)} />
+    );
   };
 
   return (
